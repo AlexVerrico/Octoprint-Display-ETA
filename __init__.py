4a4e326858c77b6bf2d47fbbdade419a88f9d701
Pablo Ventura <pablo@claude.workgroup>
2017-12-17 18:38:07 -0300

    pretty printing

--- __init__.py
+++ __init__.py
@@ -6,16 +6,41 @@ import time
 
 class HelloWorldPlugin(octoprint.plugin.ProgressPlugin,
                        octoprint.plugin.TemplatePlugin,
-                       octoprint.plugin.AssetPlugin):
+                       octoprint.plugin.AssetPlugin,
+                       octoprint.plugin.EventHandlerPlugin):
     def __init__(self):
-        self.eta_string = "nada"
-    def on_print_progress(self,storage, path, progress):
-        currentData = self._printer.get_current_data()
-        eta_strftime = "%H %M %S Day %d"
-        self.eta_string=time.strftime(eta_strftime, time.localtime(time.time() + currentData["progress"]["printTimeLeft"]))
-        self._plugin_manager.send_plugin_message(self._identifier, dict(eta_string=self.eta_string))
-        self._logger.info("Hello World! %s" % self.eta_string)
+        self.eta_string = "-"
+        self.eta_strftime = "%H:%M:%S"
 
+    def calculate_ETA(self):
+        currentData = self._printer.get_current_data()
+        if not currentData["progress"]["printTimeLeft"]:
+            return "-"
+        current_time = time.localtime(time.time())
+        finish_time = time.localtime(time.time() + currentData["progress"]["printTimeLeft"])
+        strtime = time.strftime(self.eta_strftime, finish_time)
+        strdate = ""
+        if finish_time.tm_mday > current_time.tm_mday:
+            if finish_time.tm_mday == current_time.tm_mday + 1:
+                strdate = " Tomorrow"
+            else:
+                strdate = " Day %s" % finish_time.tm_mday
+        return strtime + strdate
+            
+        
+        
+    def on_print_progress(self,storage, path, progress):
+        self.eta_string = self.calculate_ETA()
+        self._plugin_manager.send_plugin_message(self._identifier, dict(eta_string=self.eta_string))
+        
+    def on_event(self,event, payload):
+        if event.startswith('Print'):
+            if event not in {"PrintStarted","PrintResumed"}:
+                self.eta_string="-"
+            else:
+                self.eta_string = self.calculate_ETA()
+            self._plugin_manager.send_plugin_message(self._identifier, dict(eta_string=self.eta_string))
+            
     def get_assets(self):
         return {
             "js": ["js/navbartemp.js"],
