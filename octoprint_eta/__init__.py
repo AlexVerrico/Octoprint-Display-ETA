2e7ddb3e283f14f098e0cfd29adb6fcc68155725
Pablo Ventura <pablo@claude.workgroup>
2017-12-19 17:19:28 -0300

    debug

--- octoprint_eta/__init__.py
+++ octoprint_eta/__init__.py
@@ -60,6 +60,6 @@ class DisplayETAPlugin(octoprint.plugin.ProgressPlugin,
 
 __plugin_name__ = "Display ETA"
 __plugin_identifier = "display-eta"
-__plugin_version__ = "1.0.0"
+__plugin_version__ = "0.1.0"
 __plugin_description__ = "Show finish time (ETA time) for current print."
 __plugin_implementation__ = DisplayETAPlugin()
