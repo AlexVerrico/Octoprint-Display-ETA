4755fefa831927359a1f52c9cc3237451cb4ce11
Pablo Ventura <pablo@claude.workgroup>
2017-12-24 11:56:47 -0300

    usa locales para los tiempos y fechas

--- octoprint_eta/__init__.py
+++ octoprint_eta/__init__.py
@@ -4,6 +4,8 @@ from __future__ import absolute_import
 import octoprint.plugin
 from octoprint.util import RepeatedTimer
 import time
+import datetime
+from babel.dates import format_date, format_datetime, format_time
 
 class DisplayETAPlugin(octoprint.plugin.ProgressPlugin,
                        octoprint.plugin.TemplatePlugin,
@@ -12,7 +14,6 @@ class DisplayETAPlugin(octoprint.plugin.ProgressPlugin,
 
     def __init__(self):
         self.eta_string = "-"
-        self.eta_strftime = "%H:%M:%S"
         self.timer = RepeatedTimer(15.0, DisplayETAPlugin.fromTimer, args=[self], run_first=True,)
 
     def fromTimer(self):
@@ -24,15 +25,15 @@ class DisplayETAPlugin(octoprint.plugin.ProgressPlugin,
         currentData = self._printer.get_current_data()
         if not currentData["progress"]["printTimeLeft"]:
             return "-"
-        current_time = time.localtime(time.time())
-        finish_time = time.localtime(time.time() + currentData["progress"]["printTimeLeft"])
-        strtime = time.strftime(self.eta_strftime, finish_time)
+        current_time = datetime.datetime.today()
+        finish_time = current_time + datetime.timedelta(0,currentData["progress"]["printTimeLeft"])
+        strtime = format_time(finish_time)
         strdate = ""
-        if finish_time.tm_mday > current_time.tm_mday:
-            if finish_time.tm_mday == current_time.tm_mday + 1:
+        if finish_time.day > current_time.day:
+            if finish_time.day == current_time.day + 1:
                 strdate = " Tomorrow"
             else:
-                strdate = " Day %s" % finish_time.tm_mday
+                strtime = " " + format_date(finish_time,"EEE d")
         return strtime + strdate
             
         
