66a8e8fd6321ee7e664804045ca6990eb34e93d2
Alex V <41950214+AlexVerrico@users.noreply.github.com>
2020-05-07 18:55:44 +1000

    bump version to 1.0.4

--- octoprint_display_eta/static/js/displayeta.js
+++ octoprint_display_eta/static/js/displayeta.js
@@ -10,7 +10,7 @@ $(function() {
             }
         };
         self.onDataUpdaterPluginMessage = function(plugin, data) {
-            if (plugin != "octoprint_eta") {
+            if (plugin != "octoprint_display_eta") {
                 return;
             }
             self.ETA(data.eta_string);
@@ -21,7 +21,7 @@ $(function() {
     OCTOPRINT_VIEWMODELS.push([
         ETAModel, 
         ["printerStateViewModel"],
-        ["#navbar_plugin_octoprint_eta","#ETA_string"]
+        ["#navbar_plugin_octoprint_display_eta","#ETA_string"]
     ]);
 
 });
