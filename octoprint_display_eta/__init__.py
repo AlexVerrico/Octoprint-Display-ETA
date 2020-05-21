f83f1526546599225c29fd24e79723fa722a201c
Alex V <41950214+AlexVerrico@users.noreply.github.com>
2020-05-21 16:50:51 +1000

    Update __init__.py
    Add some basic debug logging functionality

--- octoprint_display_eta/__init__.py
+++ octoprint_display_eta/__init__.py
@@ -5,7 +5,10 @@ import octoprint.plugin
 from octoprint.util import RepeatedTimer
 import time
 import datetime
+import logging
 from babel.dates import format_date, format_datetime, format_time
+
+_logger = logging.getLogger('octoprint.plugins.display_eta')
 
 class DisplayETAPlugin(octoprint.plugin.ProgressPlugin,
                        octoprint.plugin.TemplatePlugin,
@@ -15,6 +18,7 @@ class DisplayETAPlugin(octoprint.plugin.ProgressPlugin,
     def __init__(self):
         self.eta_string = "-"
         self.timer = RepeatedTimer(15.0, DisplayETAPlugin.fromTimer, args=[self], run_first=True,)
+        _logger.debug('reached end of __init__')
 
     def fromTimer(self):
         self.eta_string = self.calculate_ETA()
@@ -22,6 +26,7 @@ class DisplayETAPlugin(octoprint.plugin.ProgressPlugin,
         
         
     def calculate_ETA(self):
+        _logger.debug('calculate_ETA called')
         currentData = self._printer.get_current_data()
         if not currentData["progress"]["printTimeLeft"]:
             return "--"
@@ -34,26 +39,33 @@ class DisplayETAPlugin(octoprint.plugin.ProgressPlugin,
                 strdate = " Tomorrow"
             else:
                 strtime = " " + format_date(finish_time,"EEE d")
+        _logger.debug('reached end of calculate_ETA')
         return strtime + strdate
             
         
         
     def on_print_progress(self,storage, path, progress):
+        _logger.debug('on_print_progress called')
         self.eta_string = self.calculate_ETA()
         self._printer.commands("M117 ETA is {}".format(self.eta_string))
         self._plugin_manager.send_plugin_message(self._identifier, dict(eta_string=self.eta_string))
+        _logger.debug('reached end of on_print_progress')
         
     def on_event(self,event, payload):
+        _logger.debug('on_event called')
         if event.startswith('Print'):
             if event not in {"PrintStarted","PrintResumed"}:
                 self.eta_string="---"
                 self.timer.cancel()
+                _logger.debug('event is not equal to PrintStarted or PrintResumed.')
             else:
+                _logger.debug('event is equal to PrintStarted or PrintResumed. Calling calculate_ETA')
                 self.eta_string = self.calculate_ETA()
                 self.timer.cancel()
                 self.timer = RepeatedTimer(10.0, DisplayETAPlugin.fromTimer, args=[self], run_first=True,)
                 self.timer.start()
             self._plugin_manager.send_plugin_message(self._identifier, dict(eta_string=self.eta_string))
+            _logger.debug('reached end of on_event')
             
     def get_assets(self):
         return {
