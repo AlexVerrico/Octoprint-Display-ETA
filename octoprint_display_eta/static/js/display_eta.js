6f1082134f1d19d380b9f8c7e7b66f8b0e8d30d5
Alex V <41950214+AlexVerrico@users.noreply.github.com>
2020-06-18 14:07:19 +1000

    Update display_eta.js

--- octoprint_display_eta/static/js/display_eta.js
+++ octoprint_display_eta/static/js/display_eta.js
@@ -3,42 +3,16 @@ $(function() {
         var self = this;
         self.ETA = ko.observable("-");
 		self.settings = parameters[0];
-		
 		time24hr : ko.observable();
 		displayOnPrinter : ko.observable();
-		
-		// self.updateOptions = function(){
-	        // var keys = Object.keys(self.options);
-	        // var result = [];
-	        // keys.forEach(function(key){
-	            // if (self.options[key]()) result.push("."+key);
-	        // });
-	        // $('#gcode_upload').attr('accept', result.join(','));
-	    // };
-		
         self.onBeforeBinding = function() {
             var element = $("#state").find(".accordion-inner .progress");
-			
-			// var keys = Object.keys(self.options);
-            // Initialize each element in model
-            // keys.forEach(function(key) {
-                // self.options[key](self.settings.settings.plugins.display_eta[key]());
-            // });
-            // Bind subscriber to views.
-            // keys.forEach(function(key) {
-                // self.options[key].subscribe(self.updateOptions);
-            // });
-		    // self.updateOptions();
-			
             if (element.length) {
                 // console.log("Found required elements");
                 var text = gettext("ETA");
                 // console.log(text);
                 element.before(text + ": <strong id='ETA_string' data-bind=\"html: ETA\"></strong><br>");
             }
-            /*else {
-                // console.log("could not find required elements");
-            }*/
         };
         self.onDataUpdaterPluginMessage = function(plugin, data) {
             if (plugin != "display_eta") {
@@ -47,7 +21,6 @@ $(function() {
             }
             self.ETA(data.eta_string);
         };
-
     }
 
     OCTOPRINT_VIEWMODELS.push({
@@ -55,7 +28,6 @@ $(function() {
         dependencies: ["printerStateViewModel", "settingsViewModel"],
         elements: ["#navbar_plugin_octoprint_display_eta","#ETA_string","#settings_plugin_display_eta"]
     });
-
 });
 
 
